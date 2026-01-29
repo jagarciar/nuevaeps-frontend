dc88ea00469e2a08470a21b08320f066bc2cdd88
Jeyson Andrés García Rodríguez <jeogarod@gmail.com>
2026-01-28 22:56:46 -0500

    Modificación del README.md

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,7 +9,9 @@ declare module '*.css' {
 // Declaraciones para imports de imágenes
 declare module '*.svg' {
   import * as React from 'react';
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
   const src: string;
   export default src;
 }
